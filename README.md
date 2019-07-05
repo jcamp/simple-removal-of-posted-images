c0bea91895416f9bd199152bab9594531eb79ff7
davideC00 <dvdx97@gmail.com>
2019-07-05 10:07:21 +0200

    Internationalization

--- README.md
+++ README.md
@@ -1,32 +1,37 @@
 # Simple removal of posted images
 
-* Contributors: tooulakis13
+* Contributors: colomet, tooulakis13, davideC00
 * Donate link:
 * Tags: wordpress, wordpress-plugin, images, removal, post-content-images
 * Requires at least: 4.6
-* Tested up to: 4.9.8
+* Tested up to: 5.2.2
 * Requires PHP: 5.2.4
-* Stable tag: 1.0
+* Stable tag: 1.0.1
 * License: GNU 3.0
 * License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
- 
+
 Remove all the inserted images in the content section of the posts.
- 
-## Description 
- 
-This plugin, enables you to remove all inserted images from the content of o range of posts. In the plugins menu page 
-you will find two textboxes where you can input the range of the posts IDs that you want to clear the images from its 
-content, and click the Clear Images button to remove them instantly. A clear and straight forward approach, to remove 
+
+## Description
+
+This plugin, enables you to remove all inserted images from the content of o range of posts. In the plugins menu page
+you will find two textboxes where you can input the range of the posts IDs that you want to clear the images from its
+content, and click the Clear Images button to remove them instantly. A clear and straight forward approach, to remove
 images from the content of multiple posts that makes this process much easier.
- 
-## Installation 
- 
+
+## Installation
+
 1. Upload plugin folder to /wp-content/plugins/ folder in your web-site file-system.
 2. Activate it from 'Plugins page' in your website.
 3. Access the plugins menu through the Clear Images menu page to use the plugin.
 
-## Changelog 
- 
+## Changelog
 
- 
-## Upgrade Notice 
+### 1.0.1
+* ADDITIONS
+  * Plugin internationalization
+
+### 1.0.0
+* Initial release
+
+## Upgrade Notice
--- simple-removal-of-posted-images.php
+++ simple-removal-of-posted-images.php
@@ -1,24 +1,39 @@
 <?php
 /*
   Plugin Name: Simple Removal of Posted Images
-  Plugin URI: #
+  Plugin URI: https://github.com/my-language-skills/simple-removal-of-posted-images
   Description: Clear Images from the content of a range of posts.
-  Version: 1.0
+  Version: 1.0.1
+  Author: My Language Skills team
+  Author URI: https://github.com/my-language-skills
   License: GPL-3.0
+  Text Domain: simple-removal-of-posted-images
+  Domain Path: /languages
  */
 ?>
+
 <?php
+/* Runs after the basic admin panel menu structure */
 add_action('admin_menu', 'sropi_add_clearImages_menu_page');
+/* Create the menu */
 function sropi_add_clearImages_menu_page() {
-    add_menu_page('Clear Images', 'Clear Images', 'manage_options', 'sropi_clear_post_images', 'sropi_clearImages_settings_page');
+    add_menu_page('Clear Images', __('Clear Images', 'simple-removal-of-posted-images'), 'manage_options', 'sropi_clear_post_images', 'sropi_clearImages_settings_page');
 }
+
+/**
+ * Creates the options for  'Clear Images' menu
+ *
+ * @since 1.0.0
+ * @author tooulakis13
+ *
+ */
 function sropi_clearImages_settings_page() {
     ?>
     <div class="wrap">
-        <h2>Simple Removal of Posted Images Plugin Panel</h2>
+        <h2><?php _e('Simple Removal of Posted Images Plugin Panel', 'simple-removal-of-posted-images'); ?></h2>
         <br class="clear">
         <div id="-plugin-buttons">
-            <p>You can delete images from iniside of a range of posts. Enter the ID range of the posts and press Clear Images.</p>
+            <p><?php _e('You can delete images from iniside of a range of posts. Enter the ID range of the posts and press Clear Images.', 'simple-removal-of-posted-images'); ?> </p>
             <br class="clear">
             <form action="admin.php?page=sropi_clear_post_images" method="post">
                 <?php
@@ -30,28 +45,36 @@ function sropi_clearImages_settings_page() {
                         if($from >= 0 && $to >= 0) {
                             sropi_clearImages_plugin_clearImages($from, $to);
                         } else {
-                            $newmsg = "Please enter a value greater or equal than 0.";
+                            $newmsg = __('Please enter a value greater or equal than 0.', 'simple-removal-of-posted-images');
                         echo "<script type='text/javascript'>alert('$newmsg');</script>";
                         }
                     } else {
-                        $msg = "Empty fields are not accepted. Please enter valid numbers!";
+                        $msg = __('Empty fields are not accepted. Please enter valid numbers!', 'simple-removal-of-posted-images');
                         echo "<script type='text/javascript'>alert('$msg');</script>";
                     }
                 }
                 wp_nonce_field('sropi_test_button_clicked');
                 ?>
-                <label for="sropi_fvar">From: </label>
-                <input type="number" name="sropi_fromvar" placeholder="0" min="1" step="1" />
-                <label for="sropi_tvar">To: </label>
-                <input type="number" name="sropi_tovar" placeholder="10" step="1" min="1"/>
+                <label for="sropi_fvar"> <?php _e('From', 'simple-removal-of-posted-images'); ?>: </label>
+                <input type="number" name="sropi_fromvar" placeholder="0" min="1" step="1" required/>
+                <label for="sropi_tvar"> <?php _e('To', 'simple-removal-of-posted-images'); ?> </label>
+                <input type="number" name="sropi_tovar" placeholder="10" step="1" min="1" required/>
                 <input type="hidden" value="true" name="sropi_test_button" />
-                <?php submit_button('Clear Images'); ?>
+                <?php submit_button(__('Clear Images', 'simple-removal-of-posted-images')); ?>
             </form>
             <br class="clear">
         </div>
     </div>
     <?php
 }
+
+/**
+ * Deletes images in the posts removing the <img> tag
+ *
+ * @since 1.0.0
+ * @author tooulakis13
+ *
+ */
 function sropi_clearImages_plugin_clearImages($i, $x) {
     $valuesnum = 0;
     $idarray = '';
@@ -86,7 +109,28 @@ function sropi_clearImages_plugin_clearImages($i, $x) {
         }
         $counter++;
     }
-    $message = $x . "Content Updated" . $i;
+    $message = sprintf(__('%d Content updated %d', 'simple-removal-of-posted-images'), $x, $i);
     echo "<script type='text/javascript'>alert('$message');</script>";
 }
+
+/**
+ * Internalization
+ * It loads the MO file for plugin's translation
+ *
+ * @param
+ *
+ * @since 1.0.1
+ * @author Davide Cazzorla @davideC00
+ *
+ * @return void
+ */
+	function smplads_load_plugin_textdomain() {
+    load_plugin_textdomain( 'simple-advertising', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+}
+/**
+ * Internalization
+ * Called when the activated plugin has been loaded
+ */
+add_action( 'plugins_loaded', 'smplads_load_plugin_textdomain' );
+
 ?>
