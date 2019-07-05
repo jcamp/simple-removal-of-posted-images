c6586a9df8e08235036ee22ffbb07f5e236a8107
davideC00 <dvdx97@gmail.com>
2019-07-05 13:05:51 +0200

    Remake function
    Improved the code readability of the function

--- simple-removal-of-posted-images.php
+++ simple-removal-of-posted-images.php
@@ -17,7 +17,9 @@
 add_action('admin_menu', 'sropi_add_clearImages_menu_page');
 /* Create the menu */
 function sropi_add_clearImages_menu_page() {
-    add_menu_page('Clear Images', __('Clear Images', 'simple-removal-of-posted-images'), 'manage_options', 'sropi_clear_post_images', 'sropi_clearImages_settings_page');
+    add_menu_page(__('Clear Images', 'simple-removal-of-posted-images'),
+     __('Clear Images', 'simple-removal-of-posted-images'),
+      'manage_options', 'sropi_clear_post_images', 'sropi_clearImages_settings_page');
 }
 
 /**
@@ -42,7 +44,7 @@ function sropi_clearImages_settings_page() {
                     if (isset($_POST['sropi_fromvar']) && isset($_POST['sropi_tovar']) && !empty($_POST['sropi_fromvar']) && !empty($_POST['sropi_tovar'])) {
                         $from = sanitize_text_field($_POST['sropi_fromvar']);
                         $to = sanitize_text_field($_POST['sropi_tovar']);
-                        if($from >= 0 && $to >= 0) {
+                        if($from >= 1 && $to >= 1) {
                             sropi_clearImages_plugin_clearImages($from, $to);
                         } else {
                             $newmsg = __('Please enter a value greater or equal than 0.', 'simple-removal-of-posted-images');
@@ -56,7 +58,7 @@ function sropi_clearImages_settings_page() {
                 wp_nonce_field('sropi_test_button_clicked');
                 ?>
                 <label for="sropi_fvar"> <?php _e('From', 'simple-removal-of-posted-images'); ?>: </label>
-                <input type="number" name="sropi_fromvar" placeholder="0" min="1" step="1" required/>
+                <input type="number" name="sropi_fromvar" placeholder="1" min="1" step="1" required/>
                 <label for="sropi_tvar"> <?php _e('To', 'simple-removal-of-posted-images'); ?> </label>
                 <input type="number" name="sropi_tovar" placeholder="10" step="1" min="1" required/>
                 <input type="hidden" value="true" name="sropi_test_button" />
@@ -71,46 +73,44 @@ function sropi_clearImages_settings_page() {
 /**
  * Deletes images in the posts removing the <img> tag
  *
- * @since 1.0.0
- * @author tooulakis13
+ * @param int $from start of the range
+ * @param int $to end of the range
  *
+ * @since 1.0.0
+ * @since 1.0.1.1 fixed overflow and improvements in code readability
+ * @author tooulakis13, davideC00
+ *
+ * @return void
  */
-function sropi_clearImages_plugin_clearImages($i, $x) {
-    $valuesnum = 0;
-    $idarray = '';
-    if ($i < $x) {
-        for ($i; $i <= $x; $i++) {
-            $idarray .= $i . ',';
-            $valuesnum++;
-        }
-    } else if ($x < $i) {
-        for ($x; $x <= $i; $x++) {
-            $idarray .= $x . ',';
-            $valuesnum++;
-        }
-    } else if ($i == $x) {
-        for ($i; $i <= $x; $i++) {
-            $idarray .= $i . ',';
-            $valuesnum++;
-        }
+function sropi_clearImages_plugin_clearImages($from, $to){
+  if ($from > $to){
+    //swap variables
+    $temp = $to;
+    $to = $from;
+    $from = $temp;
+  }
+
+  for($id = $from; $id <= $to; $id++){
+    //get the post by id
+    $post = get_post($id);
+    if( is_null($post) ){
+      //Stop the loop if there are no more posts
+      $to = $id;
+      break;
     }
-    $counter = 0;
-    $id_array = explode(',', $idarray);
-    while ($counter < $valuesnum) {
-        $post = get_post($id_array[$counter]);
-        if ($post->post_type == 'post') {
-            $content = wpautop($post->post_content);
-            $cont = preg_replace("/<img[^>]+\>/i", " ", $content);
-            $my_post = array(
-                'ID' => $id_array[$counter],
-                'post_content' => $cont,
-            );
-            wp_update_post($my_post);
-        }
-        $counter++;
+    else if($post->post_type == 'post'){
+      $content = wpautop($post->post_content);
+      $cont = preg_replace("/<img[^>]+\>/i", " ", $content);
+      $my_post = array(
+          'ID' => $id,
+          'post_content' => $cont,
+      );
+      wp_update_post($my_post);
     }
-    $message = sprintf(__('%d Content updated %d', 'simple-removal-of-posted-images'), $x, $i);
-    echo "<script type='text/javascript'>alert('$message');</script>";
+  }
+  $message = sprintf(__(' Content updated from %d to %d', 'simple-removal-of-posted-images'), $from, $to);
+  echo '<script type="text/javascript">alert("' . $message . '");</script>';
+
 }
 
 /**
